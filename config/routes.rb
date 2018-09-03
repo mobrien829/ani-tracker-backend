0b1cc4b6a6a1397287924906482166e1fbe26fec
github username <github email address>
2018-09-03 17:57:49 -0400

    added login route to users

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
 
+      post 'login', to: 'users#login'
+
       resources :users, only: [:index, :create, :update, :destroy]
       resources :media, only: [:index, :update, :create, :destroy]
       resources :usermedia, only: [:index, :update, :create, :destroy]
